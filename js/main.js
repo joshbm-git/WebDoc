f967c977489a9bc79454c99bebc1b93ca798aa2b
eaajome <joshuabacuriomercado@gmail.com>
2019-09-17 13:10:38 +0200

    v1.3
    fixed js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
  window.onscroll = function () {
-     var video1 = document.getElementById("video1");
-     var video2 = document.getElementById("video2");
+    var video1 = document.getElementById("video1");
+    var video2 = document.getElementById("video2");
 
-     // getBoundingClientRect checks where the element hits the value stated.
-     if (video1.getBoundingClientRect().bottom < 250 || video1.getBoundingClientRect().top > 250)
-         video1.pause();
-     else
-         video1.play();
+    // getBoundingClientRect checks where the element hits the value stated.
+    if (video1.getBoundingClientRect().bottom < 250 || video1.getBoundingClientRect().top > 250)
+        video1.pause();
+    else
+        video1.play();
 
-     if (video2.getBoundingClientRect().bottom < 250 || video2.getBoundingClientRect().top > 250)
-         video2.pause();
-     else
-         video2.play();
- }
+    if (video2.getBoundingClientRect().bottom < 250 || video2.getBoundingClientRect().top > 250)
+        video2.pause();
+    else
+        video2.play();
+}
